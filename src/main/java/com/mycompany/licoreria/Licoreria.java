a153d6a8495b0fc368bc7009bb3f23b7660ec846
Chrislight879 <christopher.vanegas@catolica.edu.sv>
2025-11-06 11:50:58 -0600

    commit init

---
+++ src/main/java/com/mycompany/licoreria/Licoreria.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.licoreria;
+
+/**
+ *
+ * @author christopher
+ */
+public class Licoreria {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
